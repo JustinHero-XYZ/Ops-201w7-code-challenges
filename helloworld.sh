a5eead9caf6f3b8264d04d6cb5fcfaf077fe0392
JustinHero-XYZ <justin.hero.2024@gmail.com>
2023-11-08 22:11:03 +0000

    gitting and committing

---
+++ helloworld.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Script Name:                  Hello World
+# Author:                       Justin
+# Date of latest revision:      11/08/2023
+# Purpose:                      Code Challenges
+
+# Declaration of variables
+
+# Declaration of functions
+
+
+# Main
+"hello world!"
+
+
+# End
