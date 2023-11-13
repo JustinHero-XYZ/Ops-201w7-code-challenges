5bef85f7df2b86e6594a5a1f050e5a77b78d33a3
JustinHero-XYZ <justin.hero.2024@gmail.com>
2023-11-13 05:45:05 +0000

    BASH arrays code challenge

--- Arrays.sh
+++ Arrays.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
-# Script Name:                  Script Name
-# Author:                       Justin Patterson
+# Script Name:                  Create Directories and Files
+# Author:                       Your Name
 # Date of latest revision:      00/00/0000
-# Purpose:                      Purpose
+# Purpose:                      Create four directories and a .txt file in each directory
 
 # Declaration of variables
+directories=("dir1" "dir2" "dir3" "dir4")
 
 # Declaration of functions
 
-
 # Main
+echo "Creating directories..."
+for dir in "${directories[@]}"; do
+    mkdir "$dir"
+    echo "Directory $dir created."
+done
 
+echo "Putting directory names into an array..."
 
+# Reference the array variable to create a new .txt file in each directory
+for dir in "${directories[@]}"; do
+    touch "$dir/file.txt"
+    echo "File.txt created in $dir."
+done
 
 # End
+
