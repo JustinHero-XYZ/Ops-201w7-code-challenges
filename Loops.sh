fcc049720e829e15f5fcdefbcaa692921cc3cb20
JustinHero-XYZ <justin.hero.2024@gmail.com>
2023-11-15 05:11:02 +0000

    loops code challenge

--- Loops.sh
+++ Loops.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-# Script Name:                  Process Manager
-# Author:                       Justin Patterson
-# Date of latest revision:      11/12/2023
-# Purpose:                      Display running processes, allow the user to kill a process by PID.
-
-# Main
+# Script Name: Process Manager
+# Author: Justin Patterson
+# Date of latest revision: 11/12/2023
+# Purpose: Display running processes, allow the user to kill a process by PID.
 
 while true; do
     # Display running processes
@@ -15,13 +13,10 @@ while true; do
     # Ask the user for a PID
     read -p $'\nEnter the PID of the process to kill (or press Ctrl + C to exit): ' pid_to_kill
 
-    # Kill the process with the given PID
-    kill -9 $pid_to_kill 2>/dev/null
-    if [ $? -eq 0 ]; then
+    # Kill the process and display the result
+    if kill -9 $pid_to_kill 2>/dev/null; then
         echo "Process with PID $pid_to_kill killed successfully."
     else
         echo "Failed to kill process with PID $pid_to_kill. Please check the PID."
     fi
 done
-
-# End
