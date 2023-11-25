0c52cdcb8dd0ae2b3740dbfacac85b65a42c792f
JustinHero-XYZ <justin.hero.2024@gmail.com>
2023-11-25 01:12:50 +0000

    Bash Practice File

---
+++ testfile.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Script Name:                  Bash Scripting Practice
+# Author:                       Your Name
+# Date of latest revision:      00/00/0000
+# Purpose:                      Bash Scripting Practice
+
+# Declaration of Array
